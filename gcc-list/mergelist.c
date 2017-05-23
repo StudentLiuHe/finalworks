d2bdf62be492aa7162eb2e7f504578b7334b27ff
StudentLiuHe <3231394682@qq.com>
2017-05-23 13:43:34 +0800

    Create mergelist.c

---
+++ gcc-list/mergelist.c
@@ -0,0 +1,58 @@
+#include "include/mergelist.h"
+#define MAX 10
+typedef struct{
+  int *elem;
+  int length;
+  int listsize;
+ }list;
+ list Initlist()
+ {  
+    list p;
+    p.elem=(int*)malloc(MAX*sizeof(int));
+    if(!p.elem){
+      exit(OVERFLOW);
+      }
+    p.length=0;
+    p.listsize=MAX;
+ }
+ int Insertlist(list L,int i,int e)
+ {  
+      if(i<1||i>L.length+1)
+           return 0;
+       int *q = L.elem[i-1];
+       int *p;
+       for(p=L.elem[L.length-1];p>=q;p--)
+            *(p+1)=*p;
+       *q=e;
+       ++L.length;
+       return 1;
+  }
+  int Deletelist(list L,int i,int e)
+  {
+      if((i<1)||(i>L.length))
+          return 0;
+      int *p,*q;
+      p=L.elem[i-1];
+      e=*p;
+      q=L.elem+L.length-1;
+      for(++p;p<=q;++p)
+      {
+          *(p-1)=*p;
+      }
+      --L.length;
+      return 1;
+ }
+ int Locatelist(list L,int e)
+ {
+    int i=1;
+    int *p;
+    p=L.elem;
+    while(i<=L.length&&e!=*(p++))
+        ++i;
+     if(i<=L.length)
+         return i;
+     return 0;
+  }
+    
+ 
+      
