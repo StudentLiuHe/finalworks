aadcfd3a4231cdd2d08b60e7a07a19ae14c245e4
StudentLiuHe <3231394682@qq.com>
2017-05-25 15:45:36 +0800

    Update main.c

--- gcc-list/main.c
+++ gcc-list/main.c
@@ -1,42 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "include/mergelist.h"
-typedef struct{
-  int *elem;
-  int length;
-  int listsize;
- }list;
 int main(void)
 {
-	list L;
-	Initlist(L);
+	Initlist();
 	printf("%d %d\n",L.length,L.listsize);
-	//test insertfunction
-	int a[5]={1,2,3,4,5},i,j;
+	int a[5]={1,2,3,4,5};
+	int i,j;
 	for(i=0;i<5;i++)
 	{
-		j=Insertlist(L,i+1,a[i]);
+		j=Insertlist(a[i]);
 		if(j==0)
 		{
 			printf("Error\n");
 		}
 	}
-	for(i=1;i<=L.length;i++)
-	{
-		printf("%d ",*(L.elem[i]));
-	}
-	//test deletefunction
-	printf("%d ",*(L.elem[2]));
-	j=Deletelist(L,2);
-	if(j==0)
-	{
-		printf("error!");
-	}
-	printf("%d \n",*(L.elem[2]));
+	printf("numbers in the list are: "); 
+	play();
+	printf("\n");
+	j=Deletelist(2);
+	printf("after delete the numbers in list are: ");
+	play();
 	//test locate
-	j=Locatelist(L,6);
+	j=Locatelist(6);
 	if(j==0)
-		printf("not found\n");
+		printf("\nnot found\n");
 	else{
 		printf("%d\n",j);
 	}
+	j=Locatelist(3);
+        if(j==0)
+		printf("\nnot found\n");
+	else{
+		printf("the index is %d\n",j);
+	}
+
 }
